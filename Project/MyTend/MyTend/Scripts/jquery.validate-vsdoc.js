fe74a6f5abcf78c314d4217c64b3ac5e79dcced3
vidrickiy <vidrickiy@mail.bars-open.ru>
2017-10-01 13:24:51 +0300

    Красоты

--- Project/MyTend/MyTend/Scripts/jquery.validate-vsdoc.js
+++ Project/MyTend/MyTend/Scripts/jquery.validate-vsdoc.js
@@ -315,9 +315,9 @@ $.extend($.validator, {
 	},
 
 	messages: {
-		required: "This field is required.",
+		required: "Заполните это поле",
 		remote: "Please fix this field.",
-		email: "Please enter a valid email address.",
+		email: "Введите правильный адрес электронной почты",
 		url: "Please enter a valid URL.",
 		date: "Please enter a valid date.",
 		dateISO: "Please enter a valid date (ISO).",
